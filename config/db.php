140df0caca53c26b3ac18d2d7cab72cb4478cf5e
Jorgixd <jbaez.241ds06@istta.edu.pe>
2025-07-23 10:54:47 -0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,12 +1,30 @@
 <?php
-$host = getenv('DB_HOST');
-$dbname = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
+class Database {
+    // --- MODIFICA ESTOS DATOS CON LOS TUYOS ---
+    private $host = 'dpg-d20edt7gi27c73ch5sbg-a';          // Host de PostgreSQL
+    private $db_name = 'cevaluacion';     // Nombre de tu base de datos
+    private $username = 'root';       // Tu usuario de PostgreSQL
+    private  $password = '2mapSsNT7OsiBwBnXousZIZVsEvoYl3H';  // Tu contraseña de PostgreSQL
+    private $port = '5432';               // Puerto (usualmente 5432)
+    // -----------------------------------------
 
-try {
-    $pdo = new PDO("pgsql:host=$host;port=5432;dbname=$dbname", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error de conexión: " . $e->getMessage());
+    private $conn;
+
+    public function connect() {
+        if ($this->conn) {
+            return $this->conn;
+        }
+
+        $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
+
+        try {
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            die('Error de Conexión: ' . $e->getMessage());
+        }
+
+        return $this->conn;
+    }
 }
+?>
